b6ceb18a1a649978d0dcf46bd75e7853762b23b2
groana <groana7@gmail.com>
2020-11-25 17:48:39 -0500

    prompts to allow location sharing

--- App.js
+++ App.js
@@ -1,51 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-//  async function getCurrentLocation() {
-//   await navigator.geolocation.getCurrentPosition(
-//       position => {
-//       let region = {
-//               latitude: parseFloat(position.coords.latitude),
-//               longitude: parseFloat(position.coords.longitude),
-//               latitudeDelta: 5,
-//               longitudeDelta: 5
-//           };
-//        return region
-    
-//       }
-      
-//   );
-// }
-  
-export default class App extends React.Component{
-  constructor(){
-    super()
+function getCurrentLocation() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    let region = {
+      latitude: parseFloat(position.coords.latitude),
+      longitude: parseFloat(position.coords.longitude),
+      latitudeDelta: 5,
+      longitudeDelta: 5,
+    };
+    return region;
+  });
+}
+
+export default class App extends React.Component {
+  constructor() {
+    super();
     this.state = {
-      initialRegion: null
-    }
+      initialRegion: null,
+    };
   }
 
- 
-   
   render() {
-
-    
-  return (
-    <MapView
-    style = {{flex : 1}}
-    provider = {PROVIDER_GOOGLE}
-    showsUserLocation
-    initialRegion = {{
-      latitude: 51.5078788,
-      longitude: -0.0877321,
-      latitudeDelta: 0.009,
-      longitudeDelta: 0.009
-    }}
-    
-    />
-  );
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={getCurrentLocation()}
+      />
+    );
   }
 }
 
