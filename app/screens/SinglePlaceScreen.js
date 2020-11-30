1df86e5f727d9c6ef6d2a129a9edbb389fcf4912
JenniferR326 <jennarafael@gmail.com>
2020-11-30 18:28:10 -0500

    pin shows up when search clicked

---
+++ app/screens/SinglePlaceScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+
+function SinglePlaceScreen(props) {
+  return (
+    <SafeAreaView>
+      <Text>{this.props.place.name}</Text>
+      <Text>This location is at 77% capacity</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+   },
+   safeArea: {
+     flex: 1,
+     backgroundColor: 'grey'
+   },
+   input : {
+     borderRadius: 4,
+     margin: 5
+    
+   }
+});
+
+export default SinglePlaceScreen;
