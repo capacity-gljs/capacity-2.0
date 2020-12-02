039bece2899af355da40580c6f59f0abaf0a955c
JenniferR326 <jennarafael@gmail.com>
2020-12-02 15:55:54 -0500

    single place screen working with master

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { Text, View, SafeAreaView, Modal, Alert, TouchableHighlight, Image} from "react-native";
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Modal,
+  Alert,
+  Button,
+  TouchableHighlight,
+  Image,
+} from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
-import {getCurrentLocation,
-         isOpen,
-         getColor,
-         getType,
-         dollarSign}
-           from './funcs'
+import {
+  getCurrentLocation,
+  isOpen,
+  getColor,
+  getType,
+  dollarSign,
+} from "./funcs";
 import { homeStyleSheet } from "./styles";
-import { db } from '../../firebase/config'
+import { db } from "../../firebase/config";
 
 export default class HomeScreen extends React.Component {
   constructor() {
@@ -23,9 +33,9 @@ export default class HomeScreen extends React.Component {
       selectedName: "",
       modalVisible: false,
       modalData: null,
-      modalDetails: null
+      modalDetails: null,
     };
-    this.setData = this.setData.bind(this)
+    this.setData = this.setData.bind(this);
   }
 
   async componentDidMount() {
@@ -33,116 +43,137 @@ export default class HomeScreen extends React.Component {
     this.setState({
       initialRegion: region,
     });
-    const places = db.collection('places')
-    const place = places.doc('ChIJrUj5NiQZBYgROOtRy0_Mnfg').collection('capacity').get().then(snap => {    snap.forEach(doc => {        console.log(doc.data());    }); })
+    const places = db.collection("places");
+    const place = places
+      .doc("ChIJrUj5NiQZBYgROOtRy0_Mnfg")
+      .collection("capacity")
+      .get()
+      .then((snap) => {
+        snap.forEach((doc) => {
+          console.log(doc.data());
+        });
+      });
 
     //const place = await places.get()
     //const foundPlace = place.forEach(doc => {
     //doc.id, '=>', doc.data()
     //})
     //console.log(foundPlace)
-    
   }
 
-  setModal(visible){
-    this.setState({modalVisible: visible})
+  setModal(visible) {
+    this.setState({ modalVisible: visible });
   }
 
-   setData(data, details, visible){
-   this.setState({
+  setData(data, details, visible) {
+    this.setState({
       modalVisible: visible,
       modalData: data,
-      modalDetails: details
-    })
+      modalDetails: details,
+    });
   }
 
-
   render() {
-    const modalVisible = this.state.modalVisible
-    const locDescription = this.state.modalDetails || ''
-    const locData = this.state.modalData || ''
-    const hours = locDescription.opening_hours || ''
-    const type = locData.types || ''
-  
-  
+    const modalVisible = this.state.modalVisible;
+    const locDescription = this.state.modalDetails || "";
+    const locData = this.state.modalData || "";
+    const hours = locDescription.opening_hours || "";
+    const type = locData.types || "";
+
     return (
-      
-     <SafeAreaView style = {homeStyleSheet.safeArea}>
-        <Modal 
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-      >
-      <View style={homeStyleSheet.modalView}>
-      <Text style={homeStyleSheet.modalName}>{locDescription.name}</Text>
-      <Text style={homeStyleSheet.modalText}>{locDescription.rating } ({locDescription.user_ratings_total})</Text>
-      <Text style={homeStyleSheet.modalType}> {getType(type)} {dollarSign(locDescription.price_level)}</Text> 
-      <Text 
-         style={{
-          marginBottom: 5, 
-          fontSize: 15,
-          color: getColor(hours)}}>{isOpen(hours)}</Text>
-        <TouchableHighlight 
-               style={{ ...homeStyleSheet.openButton, backgroundColor: "#2196F3" }}
-           onPress={() => { 
-              this.setModal(!modalVisible);
-          
-       }}
-          >
-           <Text style={homeStyleSheet.textStyle}> X </Text>
-             </TouchableHighlight> 
-          </View> 
-       </Modal> 
-       <MapView
-        ref={(map) => (this.map = map)}
-        style = {homeStyleSheet.container}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation
-        initialRegion={this.state.initialRegion}
-         >
-        {this.state.coordinates.latitude && (
-        <Marker coordinate={this.state.coordinates} onPress={()=>{
-        this.props.navigation.navigate("SinglePlace", {
-        name: this.state.selectedName
-              })
-            }}/>
-            )}
-      <GooglePlacesAutocomplete  
-      style = {homeStyleSheet.input}
-      placeholder = 'search'
-      minLength = {2}
-      fetchDetails = {true}
-      onPress={(data, details = null) => {
-       this.setData(data, details, true)
-       this.setState({
-        coordinates: {
-          latitude: details.geometry.location.lat,
-          longitude: details.geometry.location.lng,
-        },
-        selectedName: data.description
-      });
-      this.map.animateCamera({
-        center: {
-          latitude: details.geometry.location.lat,
-          longitude: details.geometry.location.lng,
-        },
-      });
-      }}
-      query={{
-          key: 'AIzaSyCukq40uCr0mkfwu4JlZaO6yQ6P0K5D7Bc',
-          language: 'en',
-        }}
-        nearbyPlacesAPI = 'GooglePlacesSearch'
-        debounce = {200}
-      />
-      </MapView> 
-     </SafeAreaView> 
-     
-     
-     );
-   }
+      <SafeAreaView style={homeStyleSheet.safeArea}>
+        <Modal animationType="slide" transparent={true} visible={modalVisible}>
+          <View style={homeStyleSheet.modalView}>
+            <Text style={homeStyleSheet.modalName}>{locDescription.name}</Text>
+            <Text style={homeStyleSheet.modalText}>
+              {locDescription.rating} ({locDescription.user_ratings_total})
+            </Text>
+            <Text style={homeStyleSheet.modalType}>
+              {" "}
+              {getType(type)} {dollarSign(locDescription.price_level)}
+            </Text>
+            <Text
+              style={{
+                marginBottom: 5,
+                fontSize: 15,
+                color: getColor(hours),
+              }}
+            >
+              {isOpen(hours)}
+            </Text>
+            <TouchableHighlight
+              style={{
+                ...homeStyleSheet.openButton,
+                backgroundColor: "#2196F3",
+              }}
+              onPress={() => {
+                this.setModal(!modalVisible);
+              }}
+            >
+              <Text style={homeStyleSheet.textStyle}> X </Text>
+            </TouchableHighlight>
+            <Text>Capacity: 77%</Text>
+            <Button
+              title="Let's go!"
+              onPress={() => {
+                console.log("Hi from onpress")
+                //this.GooglePlacesAutocompleteRef.setAddressText(""); //clears the search bar
+                console.log("GP", this.GooglePlacesAutocompleteRef)
+                this.setModal(!modalVisible);
+                this.props.navigation.navigate("SinglePlace", {
+                  name: this.state.selectedName,
+                });
+              }}
+            />
+          </View>
+        </Modal>
+        <MapView
+          ref={(map) => (this.map = map)}
+          style={homeStyleSheet.container}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation
+          initialRegion={this.state.initialRegion}
+        >
+          {this.state.coordinates.latitude && (
+            <Marker
+              coordinate={this.state.coordinates}
+              onPress={() => {
+                this.props.navigation.navigate("SinglePlace", {
+                  name: this.state.selectedName,
+                });
+              }}
+            />
+          )}
+          <GooglePlacesAutocomplete
+            style={homeStyleSheet.input}
+            placeholder="search"
+            minLength={2}
+            fetchDetails={true}
+            onPress={(data, details = null) => {
+              this.setData(data, details, true);
+              this.setState({
+                coordinates: {
+                  latitude: details.geometry.location.lat,
+                  longitude: details.geometry.location.lng,
+                },
+                selectedName: data.description,
+              });
+              this.map.animateCamera({
+                center: {
+                  latitude: details.geometry.location.lat,
+                  longitude: details.geometry.location.lng,
+                },
+              });
+            }}
+            query={{
+              key: "AIzaSyCukq40uCr0mkfwu4JlZaO6yQ6P0K5D7Bc",
+              language: "en",
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={200}
+          />
+        </MapView>
+      </SafeAreaView>
+    );
+  }
 }
-
-
-
-
