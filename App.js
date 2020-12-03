6f188357334ffb5aac5c209189b0d86b48e1dd5e
groana <groana7@gmail.com>
2020-12-03 14:50:39 -0500

    refactor: cleaned app.js

--- App.js
+++ App.js
@@ -8,20 +8,6 @@ import { createStackNavigator } from "@react-navigation/stack";
 const Stack = createStackNavigator();
 
 export default class App extends React.Component {
-  // constructor(props){
-  //   super(props)
-    
-  //   this.state = {
-  //     id: ''
-  //   }
-  //   this.onPlaceSelection = this.onPlaceSelection.bind(this)
-  // }
-
-  // // function to pass props up from homescreen to signle place screen
-  // onPlaceSelection(homeScreenState){
-  //   this.setState(homeScreenState)
-  // }
-
   render() {
     return (
       <NavigationContainer>
@@ -29,15 +15,11 @@ export default class App extends React.Component {
           <Stack.Screen 
             name="Home"
             component={HomeScreen}
-            // onPlaceSelection={this.onPlaceSelection}
-            // id={this.state.id}
           />
           <Stack.Screen
             name="SinglePlace"
             component={SinglePlaceScreen}
             options={{ title: "Location Details" }}
-            // onPlaceSelection={this.onPlaceSelection}
-            // id={this.state.id}
           />
         </Stack.Navigator>
       </NavigationContainer>
