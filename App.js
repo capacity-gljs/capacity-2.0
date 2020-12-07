9e1d29d13122226172830fe1801e4cd546e92ca6
Samantha <jathnamas@gmail.com>
2020-12-06 20:31:32 -0600

    add: adds loader screen

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import HomeScreen from './app/screens/HomeScreen';
-import SinglePlaceScreen from './app/screens/SinglePlaceScreen';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Provider } from 'react-redux';
-import SignUpScreen from './app/screens/SignUpScreen';
-import LoginScreen from './app/screens/LoginScreen';
-import store from './app/store';
+import React from "react";
+import HomeScreen from "./app/screens/HomeScreen";
+import SinglePlaceScreen from "./app/screens/SinglePlaceScreen";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Provider } from "react-redux";
+import SignUpScreen from "./app/screens/SignUpScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import Loader from "./app/screens/loader";
+import store from "./app/store";
 
 const Stack = createStackNavigator();
 
@@ -16,22 +17,28 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+            initialRouteName="Loader"
+          >
+            <Stack.Screen name="Loader" component={Loader} />
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen
               name="SinglePlace"
               component={SinglePlaceScreen}
-              options={{ title: 'Location Details' }}
+              options={{ title: "Location Details" }}
             />
             <Stack.Screen
               name="SignUp"
               component={SignUpScreen}
-              options={{ title: 'Sign Up' }}
+              options={{ title: "Sign Up" }}
             />
             <Stack.Screen
               name="Login"
               component={LoginScreen}
-              options={{ title: 'Log in' }}
+              options={{ title: "Log in" }}
             />
           </Stack.Navigator>
         </NavigationContainer>
