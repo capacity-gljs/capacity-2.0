2606bd749b20fbdd519a77648f054174e0665374
groana <groana7@gmail.com>
2020-12-09 15:33:23 -0500

    add: icon to all pages

--- App.js
+++ App.js
@@ -1,31 +1,46 @@
-import React from "react";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { Provider } from "react-redux";
-import HomeScreen from "./app/screens/HomeScreen";
-import SinglePlaceScreen from "./app/screens/SinglePlaceScreen";
-import SignUpScreen from "./app/screens/SignUpScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import UserFeedbackScreen from "./app/screens/UserFeedbackScreen";
-import store from "./app/store";
-import CameraScreen from "./app/screens/CameraScreen";
-import { Button } from "react-native";
-import Loader from "./app/screens/loader";
-import {logoutUser} from "./app/funcs/userFuncs"
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import HomeScreen from './app/screens/HomeScreen';
+import SinglePlaceScreen from './app/screens/SinglePlaceScreen';
+import SignUpScreen from './app/screens/SignUpScreen';
+import LoginScreen from './app/screens/LoginScreen';
+import UserFeedbackScreen from './app/screens/UserFeedbackScreen';
+import store from './app/store';
+import CameraScreen from './app/screens/CameraScreen';
+import { Button } from 'react-native';
+import Loader from './app/screens/loader';
+import { logoutUser } from './app/funcs/userFuncs';
+import { Ionicons } from '@expo/vector-icons';
 
 // option for drawer with no header
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from "@react-navigation/drawer";
+import { createDrawerNavigator, DrawerActions } from '@react-navigation/drawer';
 import { View, Text } from 'react-native';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+// const navigation = useNavigation();
 
 function DrawerRoutes() {
   return (
-    <Drawer.Navigator initialRouteName="Home" >
-      {/* <Stack.Screen name="Getting Started" component={Loader} /> */}
-      <Drawer.Screen name="Home" component={HomeScreen} />
+    <Drawer.Navigator initialRouteName="Home">
+      {/* <Drawer.Screen name="Home" component={HomeScreen} /> */}
+      <Drawer.Screen
+              name="Home"
+              component={DrawerRoutes}
+              options={({ navigation, route }) => ({
+                headerLeft: () => (
+                  <Ionicons
+                    name="md-menu"
+                    size={24}
+                    color="black"
+                    onPress={() => navigation.toggleDrawer()}
+                  />
+                ),
+              })}
+            />
       <Drawer.Screen name="Sign up" component={SignUpScreen} />
       <Drawer.Screen name="Log in" component={LoginScreen} />
       <Drawer.Screen name="Camera" component={CameraScreen} />
@@ -39,40 +54,100 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
         <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="Getting Started"
-          >
-            <Stack.Screen name="Getting Started" component={Loader} />
+          <Stack.Navigator initialRouteName="Getting Started">
+            <Stack.Screen 
+              name="Getting Started"
+              component={Loader}
+              options={{headerShown: false}}
+            />
             <Stack.Screen
               name="Home"
               component={DrawerRoutes}
+              // options={({ navigation, route }) => ({
+              //   headerLeft: () => (
+              //     <Ionicons
+              //       name="md-menu"
+              //       size={24}
+              //       color="black"
+              //       onPress={() => navigation.toggleDrawer()}
+              //     />
+              //   ),
+              // })}
             />
             <Stack.Screen
               name="SinglePlace"
               component={SinglePlaceScreen}
               options={({ navigation, route }) => ({
-                title: "Location Details",
+                title: 'Location Details',
+                headerLeft: () => (
+                  <Ionicons
+                    name="md-menu"
+                    size={24}
+                    color="black"
+                    // onPress={() => navigation.toggleDrawer()}
+                  />
+                ),
               })}
             />
             <Stack.Screen
               name="SignUp"
               component={SignUpScreen}
-              options={{ title: "Sign Up" }}
+              options={({ navigation, route }) => ({
+                title: 'Sign Up',
+                headerLeft: () => (
+                  <Ionicons
+                    name="md-menu"
+                    size={24}
+                    color="black"
+                    // onPress={() => navigation.toggleDrawer()}
+                  />
+                ),
+              })}
             />
             <Stack.Screen
               name="Login"
               component={LoginScreen}
-              options={{ title: "Log in" }}
+              options={({ navigation, route }) => ({
+                title: 'Log in',
+                headerLeft: () => (
+                  <Ionicons
+                    name="md-menu"
+                    size={24}
+                    color="black"
+                    // onPress={() => navigation.toggleDrawer()}
+                  />
+                ),
+              })}
             />
             <Stack.Screen
               name="Camera"
               component={CameraScreen}
-              options={{ title: "Add a Photo" }}
+              options={({ navigation, route }) => ({
+                title: 'Add a Photo',
+                headerLeft: () => (
+                  <Ionicons
+                    name="md-menu"
+                    size={24}
+                    color="black"
+                    // onPress={() => navigation.toggleDrawer()}
+                  />
+                ),
+              })}
             />
             <Stack.Screen
               name="UserFeedback"
               component={UserFeedbackScreen}
-              options={{ title: "Leave Feedback" }}
+              options={({ navigation, route }) => ({
+                title: 'Leave Feedback',
+                headerLeft: () => (
+                  <Ionicons
+                    name="md-menu"
+                    size={24}
+                    color="black"
+                    onPress={() => navigation.toggleDrawer()}
+                  />
+                ),
+              })}
             />
           </Stack.Navigator>
         </NavigationContainer>
@@ -80,85 +155,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <Provider store={store}>
-//         <NavigationContainer>
-//           <Stack.Navigator
-//             initialRouteName="Loader"
-//           >
-//             <Stack.Screen name="Loader" component={Loader} />
-//             <Stack.Screen
-//               name="Home"
-//               component={HomeScreen}
-//               options={({ navigation, route }) => ({
-//                 headerRight: () => (
-//                   store.getState().user.uid ? (
-//                     <Button
-//                       onPress={() => logoutUser()}
-//                       title="Log out"
-//                     />
-//                   ) : (
-//                     <Button
-//                       onPress={() => navigation.navigate("Login")}
-//                       title="Log in"
-//                     />
-//                   )
-//                 ),
-//                 headerLeft: () => (
-//                   <Button
-//                     onPress={() => navigation.navigate("SignUp")}
-//                     title="Sign up"
-//                   />
-//                 ),
-//               })}
-//             />
-//             <Stack.Screen
-//               name="SinglePlace"
-//               component={SinglePlaceScreen}
-//               options={({ navigation, route }) => ({
-//                 title: "Location Details",
-//                 headerRight: () => (
-//                   <Button
-//                     onPress={() => navigation.navigate("Login")}
-//                     title="Log in"
-//                   />
-//                 ),
-//               })}
-//             />
-//             <Stack.Screen
-//               name="SignUp"
-//               component={SignUpScreen}
-//               options={{ title: "Sign Up" }}
-//             />
-//             <Stack.Screen
-//               name="Login"
-//               component={LoginScreen}
-//               options={({ navigation, route }) => ({
-//                 title: "Log in",
-//                 headerRight: () => (
-//                   <Button
-//                     onPress={() => navigation.navigate("Login")}
-//                     title="Log in"
-//                   />
-//                 ),
-//               })}
-//             />
-//             <Stack.Screen
-//               name="Camera"
-//               component={CameraScreen}
-//               options={{ title: "Add a Photo" }}
-//             />
-//             <Stack.Screen
-//               name="UserFeedback"
-//               component={UserFeedbackScreen}
-//               options={{ title: "Leave Feedback" }}
-//             />
-//           </Stack.Navigator>
-//         </NavigationContainer>
-//       </Provider>
-//     );
-//   }
-// }
