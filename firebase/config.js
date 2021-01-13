76cf39133a20d43c8187eeacdee920cda44a2ce7
groana <groana7@gmail.com>
2021-01-13 18:41:56 -0500

    refactor: key file and updated README

--- firebase/config.js
+++ firebase/config.js
@@ -1,34 +1,13 @@
 import * as firebase from "firebase";
 import "@firebase/firestore";
-import {
-  FIRE_KEY,
-  AUTH_DOMAIN,
-  DATABASE_URL,
-  PROJECT_ID,
-  MESSAGE_SENDER_ID,
-  APP_ID,
-  MEASURE_ID,
-} from "../API_KEYS";
+import { FIRE_CONFIG } from "../API_KEYS";
 
-const firebaseConfig = {
-  apiKey: FIRE_KEY,
-  authDomain: AUTH_DOMAIN,
-  databaseURL: DATABASE_URL,
-  projectId: PROJECT_ID,
-  storageBucket: "capacity-gljs.appspot.com",
-  messagingSenderId: MESSAGE_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASURE_ID,
-}; // apiKey, authDomain, etc. (see above)
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(FIRE_CONFIG);
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user != null) {
     console.log("We are authenticated now!");
   }
-
-  // Do other things
 });
 
 export const db = firebase.firestore();
